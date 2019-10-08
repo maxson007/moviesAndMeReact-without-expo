704605a469ef45a1229bfc46fb07f82aab81b32e
Aymane DANIEL <serena@MacBook-Pro-de-Aymane.local>
2019-10-08 11:34:50 +0200

    ignore les warning sur ios

--- App.js
+++ App.js
@@ -5,8 +5,11 @@ import Store from './Store/configureStore'
 import { useScreens } from 'react-native-screens';
 import { persistStore } from 'redux-persist'
 import { PersistGate } from 'redux-persist/es/integration/react'
-
+import { YellowBox } from 'react-native';
 useScreens();
+
+YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader']);
+YellowBox.ignoreWarnings(['RCTRootView cancelTouches']);
 
 export default class App extends React.Component {
   render() {
